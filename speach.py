339eff8dc82110f8eb92ca352c386662b19b70ce
berserker <dima.shaman.k76@gmail.com>
2017-05-27 17:17:22 +0200

    strat of speach reconision

---
+++ speach.py
@@ -0,0 +1,35 @@
+import subprocess
+
+import speech_recognition as sr
+
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source, phrase_time_limit=5)
+
+recognize = r.recognize_google(audio)
+recognize_lower=recognize.lower()
+
+def osrun(cmd):
+    PIPE = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT)
+
+
+# try:
+#     # for testing purposes, we're just using the default API key
+#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+#     # instead of `r.recognize_google(audio)`
+#     print("Google Speech Recognition thinks you said: " + recognize)
+# except sr.UnknownValueError:
+#     print("Google Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+if "calc" in recognize_lower:
+    osrun('calc')
+if recognize_lower.startswith("open"):
+    print recognize
+else:
+    print "This is string you told: " + recognize
