aa5f3d9adf25ba1110f79ec32df11d1b0c31ffb6
berserker <dima.shaman.k76@gmail.com>
2017-05-29 19:46:39 +0200

    time, volume adjust

--- speach.py
+++ speach.py
@@ -1,20 +1,22 @@
-import os
+import os,time,re
 import platform
 import webbrowser
 import pyttsx
-
 import speech_recognition as sr
 
 
 def get_os_type():
-        return platform.system()
+    return platform.system()
 
+def current_time():
+    return time.strftime("%A %d of %b %Y year %H hours %M minutes")
 
 def speaking(text):
     engine = pyttsx.init()
-    engine.setProperty('voice','english')
-    engine.setProperty('rate',190)
-    return engine.say(text)
+    engine.setProperty('voice', 'english')
+    engine.setProperty('rate', 190)
+    engine.say(text)
+    return engine.runAndWait()
 
 
 def mute_system(_os):
@@ -37,11 +39,21 @@ def run_calculator(_os):
     elif _os == "Windows":
         return os.popen2("calc")
 
+
 def google_search(text):
     split_text = text.split(" ")
     google = "http://google.com#q="
     search_string = google + "+".join(split_text)
     return webbrowser.open_new(search_string)
+
+def adjust_volume(_os, number):
+    if type(number) == list:
+        number = "".join(number)
+    if _os == "Linux":
+        return os.popen2("amixer -D pulse sset Master " + str(number) + "%")
+    elif _os == "Windows":
+        num = (int(number) * 65535) / 100
+        return os.popen2("nircmd.exe setsysvolume " + str(num))
 
 
 def mainfunction(source):
@@ -49,12 +61,12 @@ def mainfunction(source):
     audio = r.listen(source)
     recognize = r.recognize_google(audio)
     recognize_lower = recognize.lower()
-    print(recognize)
+    #print(recognize)
     if "mute" in recognize_lower.split(" "):
         mute_system(_os)
     elif "unmute" in recognize_lower.split(" "):
         unmute_system(_os)
-    elif "calc" or "calculator" in recognize_lower.split(" "):
+    elif "calculator" in recognize_lower.split(" "):
         run_calculator(_os)
     elif recognize_lower == "open google":
         webbrowser.open_new("http://google.com")
@@ -63,15 +75,21 @@ def mainfunction(source):
         google_search(encode.partition("search for")[2])
     elif recognize_lower == "exit":
         exit()
+    elif "set volume" in recognize_lower:
+        vol_encode = "".join(recognize.encode("ascii", "ignore"))
+        adjust_volume(_os,re.findall('(\d)',vol_encode))
+    elif "time" in recognize_lower:
+        speaking(current_time())
     else:
-        print "This is string you told: " + recognize
+        print(recognize)
+        speaking("Sorry, I don't know how to proceed with " + recognize)
 
 
 if __name__ == "__main__":
     r = sr.Recognizer()
-    print("I'm listening to you!")
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
+        speaking("Hello master. I'm Listening to you.")
         while 1:
             try:
                 mainfunction(source)
