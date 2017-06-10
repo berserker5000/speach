6ba74596d8cba2ad48262e3a63dfa9eff6006a5f
berserker <dima.shaman.k76@gmail.com>
2017-06-10 15:18:05 +0200

    list of installed sw windows

--- windows_commands.py
+++ windows_commands.py
@@ -1,100 +1,5 @@
+__author__ = 'Administrator'
 import os
-from collections import namedtuple
-from ctypes import byref, create_unicode_buffer, windll
-from ctypes.wintypes import DWORD
-from itertools import count
-
-UID_BUFFER_SIZE = 39
-PROPERTY_BUFFER_SIZE = 256
-ERROR_MORE_DATA = 234
-ERROR_INVALID_PARAMETER = 87
-ERROR_SUCCESS = 0
-ERROR_NO_MORE_ITEMS = 259
-ERROR_UNKNOWN_PRODUCT = 1605
-
-PRODUCT_PROPERTIES = [u'Language',
-                      u'ProductName',
-                      u'PackageCode',
-                      u'Transforms',
-                      u'AssignmentType',
-                      u'PackageName',
-                      u'InstalledProductName',
-                      u'VersionString',
-                      u'RegCompany',
-                      u'RegOwner',
-                      u'ProductID',
-                      u'ProductIcon',
-                      u'InstallLocation',
-                      u'InstallSource',
-                      u'InstallDate',
-                      u'Publisher',
-                      u'LocalPackage',
-                      u'HelpLink',
-                      u'HelpTelephone',
-                      u'URLInfoAbout',
-                      u'URLUpdateInfo', ]
-
-Product = namedtuple('Product', PRODUCT_PROPERTIES)
-
-
-def get_property_for_product(product, property, buf_size=PROPERTY_BUFFER_SIZE):
-    property_buffer = create_unicode_buffer(buf_size)
-    size = DWORD(buf_size)
-    result = windll.msi.MsiGetProductInfoW(product, property, property_buffer, byref(size))
-    if result == ERROR_MORE_DATA:
-        return get_property_for_product(product, property, 2 * buf_size)
-    elif result == ERROR_SUCCESS:
-        return property_buffer.value
-    else:
-        return None
-
-
-def populate_product(uid):
-    properties = []
-    for property in PRODUCT_PROPERTIES:
-        properties.append(get_property_for_product(uid, property))
-    return Product(*properties)
-
-
-def get_installed_products_uids():
-    products = []
-    for i in count(0):
-        uid_buffer = create_unicode_buffer(UID_BUFFER_SIZE)
-        result = windll.msi.MsiEnumProductsW(i, uid_buffer)
-        if result == ERROR_NO_MORE_ITEMS:
-            break
-        products.append(uid_buffer.value)
-    return products
-
-
-def get_installed_products():
-    products = []
-    for puid in get_installed_products_uids():
-        products.append(populate_product(puid))
-    return products
-
-
-def is_product_installed_uid(uid):
-    buf_size = 256
-    uid_buffer = create_unicode_buffer(uid)
-    property = u'VersionString'
-    property_buffer = create_unicode_buffer(buf_size)
-    size = DWORD(buf_size)
-    result = windll.msi.MsiGetProductInfoW(uid_buffer, property, property_buffer, byref(size))
-    if result == ERROR_UNKNOWN_PRODUCT:
-        return False
-    else:
-        return True
-
-
-def generate_sw_list():
-    apps = get_installed_products()
-    application_list=set()
-    for app in apps:
-        application_list.add(app.ProductName.lower())
-    return application_list
-
-print(generate_sw_list())
 
 
 def mute_system():
@@ -114,3 +19,26 @@ def adjust_volume(number):
         number = "".join(number)
         num = (int(number) * 65535) / 100
         return os.popen2("nircmd.exe setsysvolume " + str(num))
+
+
+def run_bash_command(command):
+    com = command.split(" ")
+    main_path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
+    directory = os.walk(main_path)
+    names, output = dict(), dict()
+    for root, dirs, files in directory:
+        for f in files:
+            if f.endswith("lnk"):
+                names[f.split(".lnk")[0].lower()] = ('"' + root + f + '"')
+
+    for key, value in names.iteritems():
+        for word in com:
+            if word.lower() in key:
+                output[value] = word.lower()
+
+    for key, value in output.iteritems():
+        print("For " + value + ". I found next programs: " + key)
+
+
+run_bash_command("run fucking Anaconda viber pytty python")
+os.popen2("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Python 2.7IDLE (Python GUI).lnk")
