3802c81cccb8b10c0e74ab522e74fb071921d96e
berserker5000 <dima.shaman.k76@gmail.com>
2017-07-07 15:39:28 +0200

    V4

--- Voice assistant V4/main.py
+++ Voice assistant V4/main.py
@@ -9,48 +9,10 @@ import speech_recognition as sr
 _os = platform.system()
 
 
-class SoftwareListGenerator():
-    def Linux(self):
-        tmp_name = '/tmp/software_list_tmp.txt'
-        q, e = [], []
-        w = set()
+class GeneralProgransExecutor():
+    def executor(self, text):
+        pass
 
-        try:
-            subprocess.check_call(["apt", "list", "--installed"], stdout=open('/tmp/software_list_tmp.txt', 'wb'),
-                                  stderr=subprocess.STDOUT)
-        except Exception:
-            return "can't proceed with temp software list"
-
-        with open(tmp_name, 'r') as f:
-            for line in f:
-                q.append(line)
-            f.close()
-
-        for i in q:
-            e.append(i.split('/'))
-
-        for i in e:
-            w.add(i[0])
-        q, e = None, None
-        return w
-
-    def Windows(self):
-        main_path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-        directory = os.walk(main_path)
-        names = dict()
-        for root, dirs, files in directory:
-            for f in files:
-                if f.endswith("lnk"):
-                    names[f.split(".lnk")[0].lower()] = ('"' + root + "\\" + f + '"')
-        return names
-
-
-class ExecuteProgram():
-    def execute(self, program):
-        return os.popen2(program)
-
-
-class Time():
     def get_current_time(self):
         return time.strftime("%A %d of %b %Y year %H hours %M minutes")
 
@@ -83,11 +45,45 @@ class Listener():
 
 
 class RunProgramExecutor():
-    def __init__(self):
-        pass
+    @staticmethod
+    def __softwareLinuxGenerator(self):
+        tmp_name = '/tmp/software_list_tmp.txt'
+        q, e = [], []
+        w = set()
 
-    def execute(self):
-        pass
+        try:
+            subprocess.check_call(["apt", "list", "--installed"], stdout=open('/tmp/software_list_tmp.txt', 'wb'),
+                                  stderr=subprocess.STDOUT)
+        except Exception:
+            return "can't proceed with temp software list"
+
+        with open(tmp_name, 'r') as f:
+            for line in f:
+                q.append(line)
+            f.close()
+
+        for i in q:
+            e.append(i.split('/'))
+
+        for i in e:
+            w.add(i[0])
+        q, e = None, None
+        return w
+
+    @staticmethod
+    def __softwareWindowsGenerator(self):
+        main_path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
+        directory = os.walk(main_path)
+        names = dict()
+        for root, dirs, files in directory:
+            for f in files:
+                if f.endswith("lnk"):
+                    names[f.split(".lnk")[0].lower()] = ('"' + root + "\\" + f + '"')
+        return names
+
+    def execute(self, text):  # processing text
+        splited_text = text.split(" ")
+        return os.popen2(program)
 
     def key_words(self):
         pass
@@ -97,10 +93,7 @@ class RunProgramExecutor():
 
 
 class WebSearchExecutor():
-    def __init__(self):
-        pass
-
-    def execute(self):
+    def execute(self, text):
         pass
 
     def key_words(self):
@@ -111,10 +104,7 @@ class WebSearchExecutor():
 
 
 class SiteOpenExecutor():
-    def __init__(self):
-        pass
-
-    def execute(self):
+    def execute(self, text):
         pass
 
     def key_words(self):
@@ -123,22 +113,3 @@ class SiteOpenExecutor():
     def percentCount(self):
         pass
 
-
-class File():
-    pass
-
-
-class Internet():
-    pass
-
-
-class Main():
-    pass
-
-
-class Visual():
-    pass
-
-
-class MultiThreading():
-    pass
