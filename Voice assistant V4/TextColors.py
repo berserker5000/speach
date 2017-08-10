9ef123d237f76a2fc70f8cdf8e48ea7890542a02
berserker5000 <dima.shaman.k76@gmail.com>
2017-08-10 11:23:02 +0200

    Colors Added

--- Voice assistant V4/TextColors.py
+++ Voice assistant V4/TextColors.py
@@ -1,12 +1,14 @@
 class bcolors:
     Pink = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
+    Blue = '\033[94m'
+    Green = '\033[92m'
     Red = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    YELLOWFILL = '\033[103m'
+    Bold = '\033[1m'
+    Underline = '\033[4m'
+    YellowFill = '\033[103m'
     CrossOut = '\033[28m'
     Black = "\033[30m"
+    Yellow = "\033[33m"
+    Purple = "\033[35m"
+    Gray = "\033[90m"
